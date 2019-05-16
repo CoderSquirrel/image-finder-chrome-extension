efd2f963c16d57cbc0a937fdcfcc58699d33a1a8
Alan Schio <alan@jmjsistemas.com.br>
2019-05-16 14:56:45 -0400

    Separar aquivos por tipo e diminuir botoes

--- background.js
+++ background.js
@@ -1,14 +1,57 @@
+
+if(!Array.prototype.groupBy)
+Array.prototype.groupBy = function(fn) {
+    var self = this;
+    var me = this.map(fn);
+    me = me.filter(function(item, pos, arr) {
+        return arr.indexOf(item) == pos;
+    });
+    var g = [];
+    me.forEach(function(item) {
+        var filtered = self.filter(function(f) {
+            return fn(f) == item;
+        });
+        g.push({
+            [item]: filtered
+        });
+    });
+    return g;
+};
+
+
+var separateByType = (array) => {
+    return array.groupBy((item) => {return item.split('.')[item.split('.').length-1]});
+
+};
+
+var createText = (values) => {
+    var text = 'Arquivos Encontrados \r\n\r\n\r\n -------------------------- \r\n\r\n\r\n\r';
+    values.forEach((item)=>{
+        var type = Object.keys(item)[0];
+        text+=type;
+        text+='\r\n\r\n';
+        item[type].forEach((i)=>{
+            text+=i;
+            text+='\r\n\r\n';
+        });
+        text+='----------------------------';
+        text+='\r\n\r\n';
+    });
+    return text;
+};
+
 chrome.runtime.onMessage.addListener(
     function(arg, sender, sendResponse) {
         if(arg && arg.option && arg.option==='download') {
             if(arg.array && arg.array.length && arg.array[0].length) {
-                var text = arg.array[0].join('\n\t\n\t\n\t\n\n\t\n\t\n\t\n  \r');
+                var separated = separateByType(arg.array[0]);
+                var text = createText(separated);
                 var uri = 'data:text/plain;charset=utf-8,' + encodeURIComponent(text);
-                chrome.downloads.download({
-                    url: uri,
-                    filename: 'images.txt',
-                    saveAs: false
-                });
+                    chrome.downloads.download({
+                        url: uri,
+                        filename: 'images.txt',
+                        saveAs: false
+                    });
+                }
             }
-        }
-    });
+        });
--- popup.js
+++ popup.js
@@ -9,32 +9,32 @@ const downloadResult = (result)=> {
     console.log('result',result)
     chrome.runtime.sendMessage({option:'download', array:result});
 }
-
-findImages.addEventListener('click', () => {
-    function findImages() {
-        var images = Array.from(document.body.getElementsByTagName("img")).map((img)=> img.src);
-        console.log(images)
-        return images;
-    }
-    chrome.tabs.executeScript({
-        code: '(' + findImages + ')();'
-    },downloadResult);
-});
-
-findInBackground.addEventListener('click', () => {
-    function findInBackground() {
-        let images = Array.from(document.body.getElementsByTagName("*")).map((item) => {
-            return item.style.backgroundImage || item.style.background.indexOf('url') != -1;
-        }).filter((img) => {
-            return img && img!=null && (/url\(\"([^<]+)\".+?/i).test(img);
-        }).map((img)=>img.match(/url\(\"([^<]+)\".+?/i)[1]);
-        console.log(images)
-        return images;
-    }
-    chrome.tabs.executeScript({
-        code: '(' + findInBackground + ')();'
-    }, downloadResult);
-});
+//
+// findImages.addEventListener('click', () => {
+//     function findImages() {
+//         var images = Array.from(document.body.getElementsByTagName("img")).map((img)=> img.src);
+//         console.log(images)
+//         return images;
+//     }
+//     chrome.tabs.executeScript({
+//         code: '(' + findImages + ')();'
+//     },downloadResult);
+// });
+//
+// findInBackground.addEventListener('click', () => {
+//     function findInBackground() {
+//         let images = Array.from(document.body.getElementsByTagName("*")).map((item) => {
+//             return item.style.backgroundImage || item.style.background.indexOf('url') != -1;
+//         }).filter((img) => {
+//             return img && img!=null && (/url\(\"([^<]+)\".+?/i).test(img);
+//         }).map((img)=>img.match(/url\(\"([^<]+)\".+?/i)[1]);
+//         console.log(images)
+//         return images;
+//     }
+//     chrome.tabs.executeScript({
+//         code: '(' + findInBackground + ')();'
+//     }, downloadResult);
+// });
 
 findAll.addEventListener('click', () => {
     function findAll() {
