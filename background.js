15869dec8e9dc1dc0dfab0715dc645a9239b8c21
Alan Schio <alan@jmjsistemas.com.br>
2019-05-22 13:34:48 -0400

    Possibilidade de baixar as imagens

--- background.js
+++ background.js
@@ -1,57 +1,84 @@
-
-if(!Array.prototype.groupBy)
-Array.prototype.groupBy = function(fn) {
-    var self = this;
-    var me = this.map(fn);
-    me = me.filter(function(item, pos, arr) {
-        return arr.indexOf(item) == pos;
-    });
-    var g = [];
-    me.forEach(function(item) {
-        var filtered = self.filter(function(f) {
-            return fn(f) == item;
+if (!Array.prototype.groupBy)
+    Array.prototype.groupBy = function (fn) {
+        var self = this;
+        var me = this.map(fn);
+        me = me.filter(function (item, pos, arr) {
+            return arr.indexOf(item) == pos;
         });
-        g.push({
-            [item]: filtered
+        var g = [];
+        me.forEach(function (item) {
+            var filtered = self.filter(function (f) {
+                return fn(f) == item;
+            });
+            g.push({
+                [item]: filtered
+            });
         });
-    });
-    return g;
-};
+        return g;
+    };
 
 
 var separateByType = (array) => {
-    return array.groupBy((item) => {return item.split('.')[item.split('.').length-1]});
+    return array.groupBy((item) => {
+        return item.split('.')[item.split('.').length - 1]
+    });
 
 };
 
 var createText = (values) => {
     var text = 'Arquivos Encontrados \r\n\r\n\r\n -------------------------- \r\n\r\n\r\n\r';
-    values.forEach((item)=>{
+    values.forEach((item) => {
         var type = Object.keys(item)[0];
-        text+=type;
-        text+='\r\n\r\n';
-        item[type].forEach((i)=>{
-            text+=i;
-            text+='\r\n\r\n';
+        text += type;
+        text += '\r\n\r\n';
+        item[type].forEach((i) => {
+            text += i;
+            text += '\r\n\r\n';
         });
-        text+='----------------------------';
-        text+='\r\n\r\n';
+        text += '----------------------------';
+        text += '\r\n\r\n';
     });
     return text;
 };
 
 chrome.runtime.onMessage.addListener(
-    function(arg, sender, sendResponse) {
-        if(arg && arg.option && arg.option==='download') {
-            if(arg.array && arg.array.length && arg.array[0].length) {
-                var separated = separateByType(arg.array[0]);
-                var text = createText(separated);
-                var uri = 'data:text/plain;charset=utf-8,' + encodeURIComponent(text);
+    function (arg, sender, sendResponse) {
+        if (arg && arg.option && arg.option === 'download') {
+
+            if (arg.file) {
+                if (arg.array && arg.array.length && arg.array[0].length) {
+                    var separated = separateByType(arg.array[0]);
+                    var text = createText(separated);
+                    var uri = 'data:text/plain;charset=utf-8,' + encodeURIComponent(text);
                     chrome.downloads.download({
                         url: uri,
                         filename: 'images.txt',
                         saveAs: false
                     });
                 }
+                return;
             }
-        });
+            arg.array.forEach((item) => {
+                item.forEach((image) => {
+                    if ((/data\:image\/[^<]+;base6.+?/i).test(image)) {
+
+                        var url = image.replace(/^data:image\/[^;]+/, 'data:application/octet-stream');
+                        chrome.downloads.download({
+                        url: url,
+                        filename: ("Image."+/data\:image\/[^<]+;/i.exec(image)[0].split("/")[/data\:image\/[^<]+;/i.exec(image)[0].split("/").length-1]).replace(";", ""),
+                        saveAs: false
+                    });
+                        return;
+                    }
+
+                    chrome.downloads.download({
+                        url: image,
+                        filename: image.split('/')[image.split('/').length - 1],
+                        saveAs: false
+                    });
+                })
+
+            })
+
+        }
+    });
--- popup.js
+++ popup.js
@@ -1,46 +1,23 @@
-let findImages = document.getElementById('findImages');
-let findInBackground = document.getElementById('findInBackground');
+
 let findAll = document.getElementById('findAll');
+let download = document.getElementById('download');
 
 
 
-
-const downloadResult = (result)=> {
-    console.log('result',result)
-    chrome.runtime.sendMessage({option:'download', array:result});
+const downloadResultFile = (result)=> {
+    chrome.runtime.sendMessage({option:'download', array:result, file: true});
 }
-//
-// findImages.addEventListener('click', () => {
-//     function findImages() {
-//         var images = Array.from(document.body.getElementsByTagName("img")).map((img)=> img.src);
-//         console.log(images)
-//         return images;
-//     }
-//     chrome.tabs.executeScript({
-//         code: '(' + findImages + ')();'
-//     },downloadResult);
-// });
-//
-// findInBackground.addEventListener('click', () => {
-//     function findInBackground() {
-//         let images = Array.from(document.body.getElementsByTagName("*")).map((item) => {
-//             return item.style.backgroundImage || item.style.background.indexOf('url') != -1;
-//         }).filter((img) => {
-//             return img && img!=null && (/url\(\"([^<]+)\".+?/i).test(img);
-//         }).map((img)=>img.match(/url\(\"([^<]+)\".+?/i)[1]);
-//         console.log(images)
-//         return images;
-//     }
-//     chrome.tabs.executeScript({
-//         code: '(' + findInBackground + ')();'
-//     }, downloadResult);
-// });
+
+
+const downloadResultImages = (result)=> {
+    chrome.runtime.sendMessage({option:'download', array:result, file: false});
+}
 
 findAll.addEventListener('click', () => {
     function findAll() {
         var imgs =Array.from(document.body.getElementsByTagName("img")).map((img)=> img.src);
         let bgs = Array.from(document.body.getElementsByTagName("*")).map((item) => {
-            return item.style.backgroundImage || item.style.background.indexOf('url') != -1;
+            return getComputedStyle(item).backgroundImage || getComputedStyle(item).background.indexOf('url');
         }).filter((img) => {
             return img && img!=null && (/url\(\"([^<]+)\".+?/i).test(img);
         }).map((img)=> img.match(/url\(\"([^<]+)\".+?/i)[1]);
@@ -48,5 +25,20 @@ findAll.addEventListener('click', () => {
     }
     chrome.tabs.executeScript({
         code: '(' + findAll + ')();'
-    },downloadResult);
+    },downloadResultFile);
+});
+
+download.addEventListener('click', () => {
+    function findAll() {
+        var imgs =Array.from(document.body.getElementsByTagName("img")).map((img)=> img.src);
+        let bgs = Array.from(document.body.getElementsByTagName("*")).map((item) => {
+            return getComputedStyle(item).backgroundImage || getComputedStyle(item).background.indexOf('url');
+        }).filter((img) => {
+            return img && img!=null && (/url\(\"([^<]+)\".+?/i).test(img);
+        }).map((img)=> img.match(/url\(\"([^<]+)\".+?/i)[1]);
+        return [...imgs, ...bgs];
+    }
+    chrome.tabs.executeScript({
+        code: '(' + findAll + ')();'
+    },downloadResultImages);
 });
